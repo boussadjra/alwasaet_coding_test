6ad11a9dd471ddaba3af859b704b8342e0f2b46f
boussadjra <cb_boussadjra@esi.dz>
2023-05-19 18:04:21 +0100

    add home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Archive from '@/views/archive/index.vue'
 import ArchiveIcon from '@/components/icons/ArchiveIcon.vue'
+import Home from '@/views/home.vue'
 import Inbox from '@/views/inbox/index.vue'
 import InboxIcon from '@/components/icons/InboxIcon.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+
+    },
     {
       path: '/inbox',
       name: 'inbox',
---
+++ src/views/home.vue
@@ -0,0 +1,41 @@
+<script setup lang='ts'>
+import AppButton from '@/components/inputs/AppButton.vue';
+
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToInbox = () => {
+    router.push('/inbox');
+};
+
+</script>
+<template>
+    <div class="home">
+
+        <div class="home__hero">
+            <h1>
+                Alwasaet coding test
+            </h1>
+            <AppButton @click="goToInbox" variant="primary">Got to My Inbox</AppButton>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+}
+
+.home__hero {
+    display: grid;
+    justify-content: center;
+    gap: 12px;
+}
+</style>
